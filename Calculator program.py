c87e65863d6e82af0d1bf5dbd4424e0db27d9c1c
Sampath kumar <159629267+Sampath563@users.noreply.github.com>
2024-04-17 10:27:42 +0530

    Update Calculator program

--- Calculator program.py
+++ Calculator program.py
@@ -2,12 +2,12 @@ num1 = int(input())
 num2 = int(input())
 operator = input()[0]
 if operator == '+':
-   print(num1 + num2)
+    print(num1 + num2)
 elif operator == '-':
-   print(num1 - num2)
+    print(num1 - num2)
 elif operator == '*':
-   print(num1 * num2)
+    print(num1 * num2)
 elif operator == '/':
-   print(num1 // num2)
+    print(num1 // num2)
 else:
-   print("Invalid operator")
+    print("Invalid operator")
